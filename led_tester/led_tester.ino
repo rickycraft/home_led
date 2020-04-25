ad14cce4452a05123ced547dc7a5474f33f61691
rickycraft <ricky.roveri@gmail.com>
2020-04-25 16:37:07 +0200

    Create led_tester.ino

---
+++ led_tester/led_tester.ino
@@ -0,0 +1,26 @@
+#define LED_PIN 15
+int i = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // power on
+  digitalWrite(BUILTIN_LED, LOW);
+  while ( i < 255 ) {
+    i++;
+    analogWrite(LED_PIN, i);
+    delay(100);
+  }
+  delay(2000);
+  //power off
+  digitalWrite(BUILTIN_LED, HIGH);
+  while ( i > 0 ) {
+    i--;
+    analogWrite(LED_PIN, i);
+    delay(100);
+  }
+  delay(2000);
+}
