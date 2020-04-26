c301ffd0e8c32386f2009017338f21b2bb049a08
rick <ricky.roveri@gmail.com>
2020-04-26 11:10:04 +0200

    added led tester digitalWrite

--- led_tester/led_tester.ino
+++ led_tester/led_tester.ino
@@ -2,25 +2,29 @@
 int i = 0;
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
+    pinMode(BUILTIN_LED, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // power on
-  digitalWrite(BUILTIN_LED, LOW);
-  while ( i < 255 ) {
-    i++;
-    analogWrite(LED_PIN, i);
-    delay(100);
-  }
-  delay(2000);
-  //power off
-  digitalWrite(BUILTIN_LED, HIGH);
-  while ( i > 0 ) {
-    i--;
-    analogWrite(LED_PIN, i);
-    delay(100);
-  }
-  delay(2000);
+    // power on
+    digitalWrite(BUILTIN_LED, LOW);
+    while (i < 255) {
+        i++;
+        analogWrite(LED_PIN, i);
+        delay(100);
+    }
+    delay(2000);
+    digitalWrite(LED_PIN, HIGH);
+    delay(2000);
+    // power off
+    digitalWrite(BUILTIN_LED, HIGH);
+    while (i > 0) {
+        i--;
+        analogWrite(LED_PIN, i);
+        delay(100);
+    }
+    delay(2000);
+    digitalWrite(LED_PIN, LOW);
+    delay(2000);
 }
