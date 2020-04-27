f015c68c224dd62de432ecd4bc1d470420a71daa
rickycraft <ricky.roveri@gmail.com>
2020-04-27 12:29:57 +0200

    prism_temp -> prism_light, lib moved out

--- prism_light/include/prism.h
+++ prism_light/include/prism.h
@@ -1,39 +1,37 @@
 #include <Arduino.h>
-
 #include <ArduinoJson.h>
 #include <Espalexa.h>
-#include <WS2812FX.h>
-
-#include <MqttUtil.h>
-#include <MyDHT.h>
-#include <Util.h>
-#include <WiFiUtil.h>
+#include <MqttBroker.h>
+#include <NeoAnimationFX.h>
+#include <OtaUpdatates.h>
+#include <SensorUtil.h>
 
 // VARS
-#define LED_COUNT 53
-#define LED_PIN 0
+#define LED_COUNT 60
+#define LED_PIN 2
 // bigger is slower
 #define MAX_SPEED 2000
 #define MIN_SPEED 15000
 #define HA_SPEED 8576
+#define MAX_BRI 255
 #define ALEXA_NAME "prisma"
 #define HOSTNAME "prism"
 // json
 #define JSON_BUFFER_SIZE JSON_OBJECT_SIZE(15)
 #define CLIENT_ID "prism_light"
 #define STATE_TOPIC "prism/state"
-#define COMMAND_TOPIC "prism/set"
+char* COMMAND_TOPIC = "prism/set";
 
-MyDHT dht;
-WiFiUtil wifi;
-MqttUtil mqtt;
-Espalexa alexa;
-WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+typedef struct LedStatus {
+    bool state;
+    int lux;
+    int effect_code;
+    int speed;
+};
 
-void off();
-void update_led();
+typedef NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod> NEOMETHOD;  // uses GPIO3/RX
+
 void update_alexa(uint8_t bri);
 void publish_state();
-void callback(char *topic, byte *_payload, unsigned int _length);
-void set_effect();
-bool decodeJson(String message);
+int get_effect_code();
+void update_led();
